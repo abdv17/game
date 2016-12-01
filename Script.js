2619aed404b4bb2092e667be6802882b7233e083
abdv17 <sg8412@outlook.com>
2016-12-01 16:55:28 +0530

    all files for project
    all files for project

---
+++ Script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.first').click(function(){
+		$('.first').animate({height: "130px"});
+	});
+	$('.second').click(function(){
+		$('.second').animate({height: "30px"});	
+	});
+	$('.third').click(function(){
+		$('.third').animate({height: "130px"});
+	});
+	$('.fourth').click(function(){
+		$('.fourth').animate({height: "30px"});	
+	});
+});
